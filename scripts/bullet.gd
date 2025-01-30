e092079586439d19448738da9452f8140cdf5612
felipegabriel1942 <pinheiro_felipeg@yahoo.com.br>
2025-01-30 14:55:04 -0300

    shooting bullets

---
+++ scripts/bullet.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@export var speed = 300
+
+var bullet_rotation;
+
+func _process(delta):
+	var canon_rotation_offset = 1.55
+	var foward_direction = Vector2(0, -1).rotated(bullet_rotation)
+	
+	rotation = bullet_rotation - canon_rotation_offset	
+	position += foward_direction * speed * delta
--- scripts/cannon.gd
+++ scripts/cannon.gd
@@ -1,7 +1,23 @@
 extends Node2D
 
+@onready var bullet_container = $"../BulletContainer"
+
+const BULLET = preload("res://scenes/bullet.tscn")
+
 func get_input():
 	look_at(get_global_mouse_position())
 	
 func _physics_process(delta):
 	get_input()
+
+func _process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		_shoot()
+
+func _shoot():
+	var bullet_instance = BULLET.instantiate()
+	var canon_rotation_offset = 1.55
+	
+	bullet_instance.bullet_rotation = global_rotation + canon_rotation_offset
+	bullet_instance.global_position = global_position
+	bullet_container.add_child(bullet_instance)
